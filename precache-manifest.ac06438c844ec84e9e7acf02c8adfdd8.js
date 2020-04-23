a9eecbba056191fe8dce12477f0cbfd9c93d53cf
shefaligupta <me.shefaligupta@gmail.com>
2020-04-23 01:54:09 -0600

    serve build

---
+++ precache-manifest.ac06438c844ec84e9e7acf02c8adfdd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bba2e7ac7e195b209cea008ee74c7157",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbc1f3c1d63e4108c404",
+    "url": "/static/css/main.618dac20.chunk.css"
+  },
+  {
+    "revision": "1f4b08e3477e37dd50b9",
+    "url": "/static/js/2.1e59cc11.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.1e59cc11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbc1f3c1d63e4108c404",
+    "url": "/static/js/main.cbf17cb5.chunk.js"
+  },
+  {
+    "revision": "fc6894bd72834d5c6caf",
+    "url": "/static/js/runtime-main.7957bc1f.js"
+  }
+]);
--- src/setupTests.js
+++
@@ -1,3 +0,0 @@
-import '@testing-library/jest-dom/extend-expect';
-
-window.HTMLElement.prototype.scrollIntoView = jest.fn();
